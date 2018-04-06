9ea26a29ba2b1d7fac85271e2524506c4ba4f14a
league <league@wts19.attlocal.net>
2018-04-06 16:25:17 -0700

    Evil polygons Goofy names

--- src/EvilPolygonsChallenge.java
+++ src/EvilPolygonsChallenge.java
@@ -8,16 +8,85 @@ public class EvilPolygonsChallenge {
 	
 	public static void main(String[] args) {
 		//1. Create a robot
-
+		Robot r2d2 = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		r2d2.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "", "What is your favorite color?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Green", "Blue"}, 0);
 		
 		//3. Set the pen color of the robot according to the value of the colorChoise variable
-		
+		if(colorChoice == 0) {
+			r2d2.setPenColor(255, 0, 0);
+		}
+		if(colorChoice == 1) {
+			r2d2.setPenColor(0, 255, 0);
+		}
+		if(colorChoice == 2) {
+			r2d2.setPenColor(0, 0, 255);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		int p = JOptionPane.showOptionDialog(null, "", "How many polygons do you want to be drawn?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"1","2", "3"}, 0);
 		//5. Use the robot to draw the number of polygons the user requested.
+		if(p == 0) {
+			r2d2.penDown();
+			r2d2.setSpeed(25);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
+		if(p == 1) {
+			r2d2.penDown();
+			r2d2.setSpeed(25);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(200);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			
+		}
+		if(p == 2) {
+			r2d2.penDown();
+			r2d2.setSpeed(25);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(200);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(300);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
 		
 		//6. Make it so your shapes do not overlap
 
